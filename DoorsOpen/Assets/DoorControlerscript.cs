d3a5f48724f6d6a80793cb423055d9cf177a9f89
shaik muzammil <afranmuzammil@users.noreply.github.com>
2020-06-26 12:05:51 +0530

    Inital base build

---
+++ DoorsOpen/Assets/DoorControlerscript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DoorControlerscript : MonoBehaviour
+{
+    public float nearTodoor = 2f;
+    public Transform player;
+    public Animator Animator;
+    public AudioSource doorSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance <= nearTodoor)
+        {
+            Animator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            Animator.SetBool("character_nearby", false);
+            doorSound.Play();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, nearTodoor);
+
+    }
+}
---
+++ DoorsOpen/Assets/fanController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fanController : MonoBehaviour
+{
+    public float fanSpeed = 2f;
+    public GameObject fanBlade;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       fanBlade.transform.Rotate(0, fanSpeed * Time.deltaTime, 0);
+    }
+}
